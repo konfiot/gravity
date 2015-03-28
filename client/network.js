8f3158a9043be020619bd0be301f5dab7da9aab1
konfiot <arthurtoussaint@free.fr>
2015-03-28 19:40:32 +0100

    Restructuration

---
+++ client/network.js
@@ -0,0 +1,31 @@
+function GameClient(game, player){
+	this.game = game;
+	this.socket = io();
+	this.socket.on('e', this.update);
+	this.player = player;
+}
+
+GameClient.prototype.list = function(cb){
+	this.socket.emit("list", cb);
+}
+
+GameClient.prototype.enter = function(id, cb){
+	this.socket.emit("enter", {id: id}, cb);
+}
+
+GameClient.prototype.play = function(x,y){
+	if(this.game.play(this.player, rx,y)){ 
+		this.socket.emit("play", {x: x, y: y});
+	}
+}
+
+GameClient.prototype.update = function(e){
+	switch(e.action){
+		case "play":
+			this.game.play(e.player, e.x, e.y);
+		break;
+		case "update":
+			this.game.update(e.state);
+		break;
+	}
+}
