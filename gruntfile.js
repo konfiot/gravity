a8179a6a54b5056bd7361fa9083fb1abdedcd3b2
konfiot <arthurtoussaint@free.fr>
2015-03-28 19:48:56 +0100

    Debut de serveur plus compilation

---
+++ gruntfile.js
@@ -0,0 +1,114 @@
+/*jslint node: true */
+module.exports = function (grunt) {
+    'use strict';
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            js: {
+                src: ['node_modules/socket.io-client/socket.io.js',  'common/game.js', 'client/js/main.js', 'client/js/network.js'],
+                dest: 'client/dist.js'
+            },
+            css: {
+                src: ['client/css/main.css'],
+                dest: 'client/main.css'
+            },
+        },
+        uglify : {
+            main: {
+                src: "client/dist.js",
+                dest: "client/dist.js"
+            },
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'client/index.html': ['client/html/index.html'],
+                }
+            }
+        },
+        cssmin: {
+            main: {
+                src: 'client/main.css',
+                dest: 'client/main.css'
+            },
+        },
+        copy: {
+            html: {
+                expand: true,
+                cwd: 'client/html',
+                src: '*.html',
+                dest: 'client/'
+            }
+        },
+        csslint : {
+            options: {
+                "ids": false
+            },
+            src: ["client/css/*.css"]
+        },
+        htmllint: {
+            all: ["client/html/*.html"]
+        },
+        jslint: {
+            all: ['gruntfile.js', 'common/**/*.js', 'server/*.js', 'client/js/*.js']
+        },
+        nodeunit: {
+            all: ['test/test-*.js']
+        },
+        replace: {
+            dist: {
+                options: {
+                    patterns: [{
+                        match: 'URL_SOCKETIO_SERVER',
+                        replacement: process.env.SOCKETIO_SERVER || ''
+                    }]
+                },
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: ['client/*.js'],
+                    dest: 'client'
+                }]
+            }
+        },
+        exec: {
+            server : {
+                command: "node server/app.js"
+            }
+        },
+        inline: {
+            desktop: {
+                src: ['client/index.html']
+            },
+            mobile: {
+                src: ['client/mobile.html']
+            }
+        },
+        clean: {
+            dist: ["client/*.css", "client/*.js"]
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-jslint');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-html');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-inline');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('default', ['concat', "replace", 'uglify', "htmlmin", "cssmin", "inline", "clean"]);
+    grunt.registerTask('dev', ['concat', 'copy', 'replace', 'inline', 'clean', 'exec']);
+    grunt.registerTask('test', ['jslint', 'csslint', 'htmllint', 'nodeunit', 'default']);
+};
---
+++ server/app.js
@@ -0,0 +1,55 @@
+/*jslint node: true*/
+
+var sio = require('socket.io'),
+    uuid = require('node-uuid'),
+    http = require('http'),
+    fs = require('fs'),
+    zlib = require("zlib"),
+
+var server = http.createServer(function (request, response) {
+    'use strict';
+    var filename = "index.html",
+        encoding = "identity",
+        acceptEncoding = request.headers['accept-encoding'],
+        raw;
+
+    if (!acceptEncoding) {
+        acceptEncoding = '';
+    }
+
+    /*jslint nomen: true */
+    raw = fs.createReadStream(__dirname + '/../client/' + filename);
+    /*jslint nomen: false */
+
+    if (acceptEncoding.match(/\bgzip\b/)) {
+        encoding =  'gzip';
+        raw = raw.pipe(zlib.createGzip());
+    } else if (acceptEncoding.match(/\bdeflate\b/)) {
+        encoding = "deflate";
+        raw = raw.pipe(zlib.createDeflate());
+    }
+
+    if (request.url === '/') {
+        response.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Cache-Control': 'max-age=' + 86400000 * 7,
+            'content-encoding': encoding
+        });
+        raw.pipe(response);
+    } else {
+        response.writeHead(404);
+        response.end();
+    }
+});
+
+server.listen(parseInt(process.env.PORT || 1337, 10));
+
+var io = sio.listen(server);
+
+
+/* ---------------------------- Listening sockets ---------------------------- */
+
+io.sockets.on('connection', function (socket) {
+	'use strict';
+});
+
