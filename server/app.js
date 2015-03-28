a05b2ef4021b476e69761b715d6abb458db8120a
Arthur <spam@crana.tk>
2015-03-28 20:06:21 +0000

    Premiere implémentation du serveur

--- server/app.js
+++ server/app.js
@@ -1,55 +1,79 @@
 /*jslint node: true*/
 
 var sio = require('socket.io'),
-    uuid = require('node-uuid'),
-    http = require('http'),
-    fs = require('fs'),
-    zlib = require("zlib");
+	uuid = require('node-uuid'),
+	http = require('http'),
+	fs = require('fs'),
+	zlib = require("zlib"),
+	Game = require("../common/game.js");
 
 var server = http.createServer(function (request, response) {
-    'use strict';
-    var filename = "index.html",
-        encoding = "identity",
-        acceptEncoding = request.headers['accept-encoding'],
-        raw;
+	'use strict';
+	var filename = "index.html",
+		encoding = "identity",
+		acceptEncoding = request.headers['accept-encoding'],
+		raw;
 
-    if (!acceptEncoding) {
-        acceptEncoding = '';
-    }
+	if (!acceptEncoding) {
+		acceptEncoding = '';
+	}
 
-    /*jslint nomen: true */
-    raw = fs.createReadStream(__dirname + '/../client/' + filename);
-    /*jslint nomen: false */
+	/*jslint nomen: true */
+	raw = fs.createReadStream(__dirname + '/../client/' + filename);
+	/*jslint nomen: false */
 
-    if (acceptEncoding.match(/\bgzip\b/)) {
-        encoding =  'gzip';
-        raw = raw.pipe(zlib.createGzip());
-    } else if (acceptEncoding.match(/\bdeflate\b/)) {
-        encoding = "deflate";
-        raw = raw.pipe(zlib.createDeflate());
-    }
+	if (acceptEncoding.match(/\bgzip\b/)) {
+		encoding =  'gzip';
+		raw = raw.pipe(zlib.createGzip());
+	} else if (acceptEncoding.match(/\bdeflate\b/)) {
+		encoding = "deflate";
+		raw = raw.pipe(zlib.createDeflate());
+	}
 
-    if (request.url === '/') {
-        response.writeHead(200, {
-            'Content-Type': 'text/html',
-            'Cache-Control': 'max-age=' + 86400000 * 7,
-            'content-encoding': encoding
-        });
-        raw.pipe(response);
-    } else {
-        response.writeHead(404);
-        response.end();
-    }
+	if (request.url === '/') {
+		response.writeHead(200, {
+			'Content-Type': 'text/html',
+			'Cache-Control': 'max-age=' + 86400000 * 7,
+			'content-encoding': encoding
+		});
+		raw.pipe(response);
+	} else {
+		response.writeHead(404);
+		response.end();
+	}
 });
 
 server.listen(parseInt(process.env.PORT || 1337, 10));
 
 var io = sio.listen(server);
 
+var 	games_pending = {},
+	running_games = {};
 
 /* ---------------------------- Listening sockets ---------------------------- */
 
 io.sockets.on('connection', function (socket) {
 	'use strict';
+	socket.on("list", function (data, cb) {
+		cb(games_pending);
+	});
+	socket.on("create", function (data, cb) {
+		var id = uuid.v4();
+		games_pending[id] = {name: data["name"]};
+		socket.player = 1;
+		cb({id: id, player: socket.player});
+	});
+	socket.on("enter", function (data, cb) {
+		running_games[data.id] = {name: games_pending.name, game: new Game(9, function(){})};
+		socket.player = 2;
+		delete games_pending[data.id];
+		cb({player: socket.player});
+	});
+	socket.on("play", function (data, cb) {
+		cb(running_games[data.id].game.play(data.x, data.y, socket.player));
+		if (running_games[data.id].game.isfinished()){
+			delete running_games[data.id];
+		}
+	});
 });
 
