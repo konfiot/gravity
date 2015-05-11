bc7e6e368470633827624149f7105e88b90c3bd0
Romaingin <romain.ginestou@gmail.com>
2015-05-11 23:27:42 +0200

    Vers l'infini et au-delà !

--- client/js/ai/ai_bob.js
+++ client/js/ai/ai_bob.js
@@ -6,7 +6,7 @@
 			* -1 : milieu
 			* 1..4 : case prise par le joueur 1..4
 		- scores : un tableau des scores, l'indice i contient le score du joueur i
-		- played : la liste des segments déjà jouées, sous la forme [cases, direction], avec cases la liste des cases contenues dans le segment, et direction la direction du segment :
+		- played : la liste des segments déjà joués, sous la forme [cases, direction], avec cases la liste des cases contenues dans le segment, et direction la direction du segment :
 			- 0 : ligne
 			- 1 : colonne
 			- 2 : diagonale Strasbourg Bordeaux
@@ -14,5 +14,6 @@
 */
 
 function iaplay_bob (state, scores, played) {
+	console.log(checkplay(state,0,0))
 	return [parseInt(Math.random() * state.length), parseInt(Math.random() * state.length)];
 }
