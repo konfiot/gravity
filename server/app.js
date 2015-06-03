69854851a93dbf7d16de001614f5a78b97755a6f
konfiot <arthurtoussaint@free.fr>
2015-06-03 19:51:21 +0200

    Wsh on inclut pas ce qu'il faut pas

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 var	sio = require("socket.io"),
 	http = require("http"),
 	fs = require("fs"),
-	zlib = require("zlib"),
 	uuid = require("uuid"),
 	Game = require("../common/game.js").Game,
 	scoring = require("./scoring.js");
