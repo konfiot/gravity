9795df0a1bff4c752bf6282247aabae092d64fa1
Lucas <lucas.pluvinage@gmail.com>
2015-12-02 22:23:45 +0000

    Affichage des coups jouables et quelques modifs graphiques

--- client/js/main.js
+++ client/js/main.js
@@ -16,7 +16,7 @@ function toggle_div(name, show) {
 	document.getElementById(name).style.display = (show) ? "block" : "none";
 }
 
-function update(state, score, finished, current, plays, lastplays) {
+function update(game, state, score, finished, current, plays, lastplays) {
 	var cells = document.getElementById("game").getElementsByTagName("td");
 
 	console.log("Updated", score, current, current % score[0].length);
@@ -29,6 +29,10 @@ function update(state, score, finished, current, plays, lastplays) {
 
 		} else {
 			cells[i].id = "";
+		}
+
+		if(cells[i].className == "p0" && !game.checkplay(cells[i].parentElement.rowIndex,cells[i].cellIndex)) {
+			cells[i].className = "g";
 		}
 
 		for (var j = 0; j < plays.length; j += 1) {
@@ -111,8 +115,8 @@ document.getElementById("solo_config").addEventListener("submit", function (e) {
 		}
 	}
 
-	var game = new Game(size, function (state, score, finished, current, plays, lastplays) {
-		update(state, score, finished, current, plays, lastplays);
+	var game = new Game(size, function (tgame, state, score, finished, current, plays, lastplays) {
+		update(tgame, state, score, finished, current, plays, lastplays);
 
 		if (players[game.whosturn] >= 2) {
 			switch (players[game.whosturn]) {
